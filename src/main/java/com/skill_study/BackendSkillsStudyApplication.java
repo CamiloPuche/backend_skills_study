6220fc2adaf8cb01b75404f92875a027792cc010
CamiloPuche <camilopuche73@gmail.com>
2023-09-21 10:55:39 -0500

    Creacion y configuracion del proyecto

---
+++ src/main/java/com/skill_study/BackendSkillsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.skill_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSkillsStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSkillsStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skill_study/BackendSkillsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skill_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSkillsStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
